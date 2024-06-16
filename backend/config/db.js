8ca60c336ad011b827230e63fb5fa7da3dec0df6
Pushparaj K <110168853+Pushparaj96@users.noreply.github.com>
2024-06-16 12:01:15 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,6 @@ import mongoose from "mongoose";
 
 
 export const connectDB= async ()=>{
-    await mongoose.connect('mongodb+srv://pushparaj:Hungerrr@atlascluster.jzwha7v.mongodb.net/Hungerrr').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongoose connection string').then(()=>console.log("DB Connected"));
+    // enter mongoose connection string to test backend
 }
